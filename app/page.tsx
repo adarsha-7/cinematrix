9bd79961801740d37d849a31f2cf07fbf7de8ade
Adarsha Ghimire <adarshghimire2005@gmail.com>
2025-11-19 21:35:10 +0545

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <main>
+            <h1 className="text-center text-3xl mt-10">Hello, World!</h1>
+            <h1 className="text-center text-3xl mt-5">Welcome To Cinematrix</h1>
+        </main>
+    );
+}

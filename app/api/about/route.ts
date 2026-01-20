46d2bef08142d7d926c2d1e6570a4c6b9172eaf8
SodiumZincate <144541256+SodiumZincate@users.noreply.github.com>
2026-01-20 21:21:36 +0545

    Update route.ts (Minor bug fix)

--- app/api/about/route.ts
+++ app/api/about/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export function GET(req: NextRequest) {
+export function GET(req:NextRequest) {
     const aboutText =
         'Cinematrix is a web based movie catalogue where users can browse, maintain watchlists and rate movies and the app provides personalized movie recommendations to the user based on their ratings and activities.';
     return NextResponse.json({

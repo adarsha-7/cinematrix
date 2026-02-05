7db0a64e9119441412e48f76947ca9da5f06922f
Adarsha Ghimire <adarshghimire2005@gmail.com>
2026-02-05 21:34:52 +0545

    email bug

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,24 +1,26 @@
 import { MailtrapClient } from 'mailtrap';
 
-const TOKEN = process.env.MAILTRAP_API_KEY as string;
-
-const client = new MailtrapClient({
-    token: TOKEN,
-});
-
 const sender = {
     email: 'cinematrix@adarshaghimire.com.np',
     name: 'Cinematrix',
 };
 
 export async function sendMail(recipients: { email: string }[], subject: string, text: string) {
-    client
-        .send({
+    try {
+        const client = new MailtrapClient({
+            token: process.env.MAILTRAP_API_KEY as string,
+        });
+
+        await client.send({
             from: sender,
             to: recipients,
             subject,
             text,
-        })
-        .then(() => console.log('Email send successfully.'))
-        .catch((err) => console.error('Error sending email', err));
+        });
+
+        console.log('Email sent successfully');
+    } catch (err) {
+        console.error('Error sending email', err);
+        throw err;
+    }
 }
